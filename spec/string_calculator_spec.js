1ca94fb6c4ea0222f64405061b242fe19078252c
mesha <mesha@KATIJA>
2026-05-18 18:45:54 +0200

    dritter Testfall druchgeführt & bestanden.

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -10,4 +10,7 @@ describe('findSpaceship', function() {
 	it('should return [1,0] for .X', function() {
         expect(calculator.add('.X')).toEqual([1,0]);
     });
+	it('should return [2,0] for ..X', function() {
+        expect(calculator.add('..X')).toEqual([2,0]);
+    });
 });

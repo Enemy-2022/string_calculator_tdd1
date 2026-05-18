b5676e6b66a8b76905eb2c73a7917ab2319fdfd7
mesha <mesha@KATIJA>
2026-05-18 19:44:19 +0200

    vierter Testfall getestet und bestanden

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -13,7 +13,9 @@ describe('findSpaceship', function() {
 	it('should return [2,0] for ..X', function() {
         expect(calculator.add('..X')).toEqual([2,0]);
     });
-
+	it('should return [0,1] for X\\n.', function() {
+        expect(calculator.add('X\n.')).toEqual([0,1]);
+    });
 
 	it('should return [0,0] for ...\\nX..', function() {
         expect(calculator.add('...\nX..')).toEqual([0,0]);
